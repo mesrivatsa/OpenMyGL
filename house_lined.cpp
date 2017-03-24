9d4e1d3c001e91ba291ab278282c8eca6a1a411b
Srivatsa Sharma G <Srivatsasharmagubbi@gmail.com>
2017-03-24 22:24:47 +0530

    Update and rename house_lined to house_lined.cpp

--- house_lined.cpp
+++ house_lined.cpp
@@ -144,7 +144,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500,500);
     glutInitWindowPosition(0,0);
-    glutCreateWindow("House Rotation");
+    glutCreateWindow("Srivatsa's house rotation.");
     glutDisplayFunc(display);
     myinit();
     glutMainLoop();
