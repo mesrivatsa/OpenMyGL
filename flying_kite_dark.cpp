bf15ef9d51d5ed998a1cd6e5aab0d26723a90aee
Srivatsa Sharma G <Srivatsasharmagubbi@gmail.com>
2017-03-24 22:25:19 +0530

    Update and rename flying_kite_dark to flying_kite_dark.cpp

--- flying_kite_dark.cpp
+++ flying_kite_dark.cpp
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(800,800);
-    glutCreateWindow("Dream_kite");
+    glutCreateWindow("Srivatsa's dream kite!");
     jinit();
     glutDisplayFunc(pyramid);
     glutMainLoop();
